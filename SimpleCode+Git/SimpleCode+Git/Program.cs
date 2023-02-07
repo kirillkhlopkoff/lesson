20bbddde6a2ad41c4430a45d4b7e1b98f2d1ac45
kirillkhlopkoff <kirillkhlopkoff@gmail.com>
2023-02-07 16:54:19 +0200

    рекурсивный вывод массива

--- SimpleCode+Git/SimpleCode+Git/Program.cs
+++ SimpleCode+Git/SimpleCode+Git/Program.cs
@@ -1,8 +1,21 @@
 ﻿class Program
 {
+    static void Foo(int i, int length)
+    {
+        Console.WriteLine(i);
+        if(i<= length)
+        {
+            i++;
+            Foo(i, length);
+        }
+        
+        
+    }
     static void Main(string[] args)
     {
-
+        int[] arr = new int[4] { 1, 5, 9, 0 };
+        int i = 0;
+            Foo(arr[i], arr.Length);
     }
 }
 
