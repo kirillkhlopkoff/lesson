956aad68666ef20221bc81138d708ea8f17b3071
kirillkhlopkoff <114176969+kirillkhlopkoff@users.noreply.github.com>
2023-02-08 15:12:32 +0200

    Вывод суммы элементов массива с помощью рекурсивного метода

--- SimpleCode+Git/SimpleCode+Git/Program.cs
+++ SimpleCode+Git/SimpleCode+Git/Program.cs
@@ -1,8 +1,19 @@
 ﻿class Program
 {
+    static int Sum(int[] myArray, int i = 0)
+    {
+        if(i>= myArray.Length)
+        {
+            return 0;
+        }
+        int result = Sum(myArray, i + 1);
+        return myArray[i] + result;
+    }
     static void Main(string[] args)
     {
-        
+        int[] myArray = { 2, 6, 12, 22, 14 };
+        int result = Sum(myArray);
+        Console.WriteLine(result);
     }
 }
 
